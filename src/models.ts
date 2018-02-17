ed9e2c26122243c73d7105e50d0543711bfd2e8b
Juraj Mičko <micko.juraj64@gmail.com>
2018-02-17 19:02:28 +0000

    Models

--- src/models.ts
+++ src/models.ts
@@ -1,7 +1,31 @@
+class Message {
+    public time: number;
 
-export class UpdateMessage {
-    public constructor(public time: number) {
+    public constructor(public device_id: string) {
+        this.time = new Date().getTime();
+    }
+}
 
+class AnchoredScreen {
+    public constructor(public x: number,
+                       public y: number,
+                       public rotation: number) {
+    }
+}
+
+export class UpdateMessage extends Message {
+    public constructor(device_id: string,
+                       public map: AnchoredScreen[],
+                       public localScreen: AnchoredScreen) {
+        super(device_id);
+    }
+}
+
+export class SwipeMessage extends Message {
+    public constructor(device_id: string,
+                       public edge: number,
+                       public distance: number) {
+        super(device_id);
     }
 }
 
